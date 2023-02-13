0edead14d8fdc9d2eb8744b0646e41c21dade039
Gleason <bianliuzhu@gmail.com>
2023-02-13 21:57:56 +0800

    feat: 忽略文件

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('res');
+import { program } from 'commander';
+
+program
+	.version(`${require('../package.json').version}`, '-v --version')
+	.usage('<command> [options]');
+
+program
+	.command('lint')
+	.description('初始化 eslint, prettier, commitlint')
+	.action(() => {
+		console.log('初始化');
+	});
+
+program.parse(process.argv);
